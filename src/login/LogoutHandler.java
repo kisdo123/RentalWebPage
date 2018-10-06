0f7842600c8851eafdd6f4521b4fbcaacd687f3e
김성현 <40026862+rmfnwldi@users.noreply.github.com>
2018-10-06 14:34:25 +0900

    logout Handler class add

---
+++ src/login/LogoutHandler.java
@@ -0,0 +1,29 @@
+package login;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import main.handler.CommandHandler;
+
+public class LogoutHandler implements CommandHandler{
+	
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		// 세션 객체를 생성
+		HttpSession session = req.getSession(false);
+		
+		if(session != null) {			
+			// 세션을 날리고!
+			session.invalidate();
+		}
+				
+		// 리다이렉트로 돌려줌
+		resp.sendRedirect(req.getContextPath() + "/loginIndex.jsp");
+		return null;
+	}
+	
+}
